6f2d06441e27284394c6815a2b4a95971177c62e
asc4real <ramnarayana1998@gmail.com>
2020-09-18 16:16:40 +0530

    Fixed Compiler Errors

--- src/app/components/carouselgrid.jsx
+++ src/app/components/carouselgrid.jsx
@@ -20,16 +20,16 @@ export default function PicPlayer() {
         <div style={{ paddingTop:'38px' }}>
         <Slider {...settings} style={{ maxWidth: '400px', maxHeight: '300px' }}>
           <div>
-            <img style={{ maxWidth: '400px', maxHeight: '300px' }} src='https://scontent.frja1-1.fna.fbcdn.net/v/t1.0-9/119604964_172347614472870_8855896629420420454_o.jpg?_nc_cat=101&_nc_sid=8024bb&_nc_ohc=9pF4r8pRv4AAX84Sefy&_nc_ht=scontent.frja1-1.fna&oh=0bcf9aac0d3e6539f30b4e19f171790d&oe=5F863972' />
+            <img style={{ maxWidth: '400px', maxHeight: '300px' }} src='https://scontent.frja1-1.fna.fbcdn.net/v/t1.0-9/119443202_2689440884630899_5462873735463640855_n.jpg?_nc_cat=108&_nc_sid=8bfeb9&_nc_ohc=JttaMAmAUVEAX_qKy-N&_nc_ht=scontent.frja1-1.fna&oh=cff07b1fd9df3fbd9752c11004a34f05&oe=5F88E9CE' />
           </div>
           <div>
-            <img style={{ maxWidth: '400px', maxHeight: '300px' }} src='https://scontent.frja1-1.fna.fbcdn.net/v/t1.0-9/119179591_172347607806204_8393668315636272082_o.jpg?_nc_cat=109&_nc_sid=8024bb&_nc_ohc=f4RFoAfO7oEAX9LHDkH&_nc_ht=scontent.frja1-1.fna&oh=516d865779ac698ba8fed0d8d0d528b4&oe=5F883C9E' />
+            <img style={{ maxWidth: '400px', maxHeight: '300px' }} src='https://scontent.frja1-1.fna.fbcdn.net/v/t1.0-9/119438071_2689440934630894_1864643428185660904_n.jpg?_nc_cat=100&_nc_sid=8bfeb9&_nc_ohc=orNsA_TIGXUAX_xIxx7&_nc_ht=scontent.frja1-1.fna&oh=9bcf884dba579f6a08174e4c9a56c379&oe=5F8BDB4C' />
           </div>
           <div>
-            <img style={{ maxWidth: '400px', maxHeight: '300px' }} src='https://scontent.frja1-1.fna.fbcdn.net/v/t1.0-9/119182496_172347601139538_6071772174696369417_o.jpg?_nc_cat=111&_nc_sid=8024bb&_nc_ohc=7ACsulSCetQAX9FL94K&_nc_ht=scontent.frja1-1.fna&oh=ee63c0b02d34b8ad5a47dd28d979090a&oe=5F8761D6' />
+            <img style={{ maxWidth: '400px', maxHeight: '300px' }} src='https://scontent.frja1-1.fna.fbcdn.net/v/t1.0-9/119621978_2689440964630891_111252681990387992_n.jpg?_nc_cat=105&_nc_sid=8bfeb9&_nc_ohc=LnWCVNpkFxcAX-UWZ9t&_nc_ht=scontent.frja1-1.fna&oh=365faaf49bef42e93895b35a06384fa7&oe=5F8C0743' />
           </div>
           <div>
-            <img style={{ maxWidth: '400px', maxHeight: '300px' }} src='https://scontent.frja1-1.fna.fbcdn.net/v/t1.0-9/119471331_172347664472865_8970185411929659979_o.jpg?_nc_cat=107&_nc_sid=8024bb&_nc_ohc=mpcJhRnu_KIAX8T9YXe&_nc_ht=scontent.frja1-1.fna&oh=0e7105c7a9842172d9659091efe14700&oe=5F86F8EE' />
+            <img style={{ maxWidth: '400px', maxHeight: '300px' }} src='https://scontent.frja1-1.fna.fbcdn.net/v/t1.0-9/119528595_2689440871297567_7457070299950877393_n.jpg?_nc_cat=111&_nc_sid=8bfeb9&_nc_ohc=_VkLMzWc6h0AX_LhrKD&_nc_oc=AQle1PzHDYINAU0vgEr17lpS-nIhTbtxRxx1j-F_oNU3LC2Np3DPowNxyorlgOX-Q1I&_nc_ht=scontent.frja1-1.fna&oh=9fee5309cc5b96a296df4001fb0bb685&oe=5F886EE6' />
           </div>
         </Slider>
       </div>

c7824993efd1198c388a6e742c48f3a01006a975
asc4real <ramnarayana1998@gmail.com>
2020-09-21 15:54:54 +0530

    Timeline code change

--- src/app/components/TimeLine.jsx
+++ src/app/components/TimeLine.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { Timeline } from 'antd';
 
 export default function TimeLine() {
+
+  const sampleData = ["Create a services site 2015-09-01",
+                      "Solve initial network problems 2015-09-01",
+                      "Technical testing 2015-09-01",
+                      "Network problems being solved 2015-09-01"];
+
+  
+
   return (
     <div>
+      
         <Timeline>
-            <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-            <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
-            <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
-            <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+        {sampleData.map((sample_data) =>
+            <Timeline.Item>{sample_data}</Timeline.Item>
+            )} 
         </Timeline>
+        
     </div>
   );
 }
